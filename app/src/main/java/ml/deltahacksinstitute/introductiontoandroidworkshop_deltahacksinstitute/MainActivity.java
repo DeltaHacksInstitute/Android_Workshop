a2fcd28fe5b2e07d227d3c319af70207ef615642
Tom Zhang <zhangtom54321@gmail.com>
2018-09-07 19:29:18 -0700

    Added Android Studio getting started

---
+++ app/src/main/java/ml/deltahacksinstitute/introductiontoandroidworkshop_deltahacksinstitute/MainActivity.java
@@ -0,0 +1,13 @@
+package ml.deltahacksinstitute.introductiontoandroidworkshop_deltahacksinstitute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
