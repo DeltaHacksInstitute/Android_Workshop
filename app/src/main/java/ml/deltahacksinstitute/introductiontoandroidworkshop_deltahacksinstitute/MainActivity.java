058e945403e205622da12f9b719c5d0b317f8450
Tom Zhang <zhangtom54321@gmail.com>
2018-09-08 17:29:52 -0700

    Finished hello world

--- app/src/main/java/ml/deltahacksinstitute/introductiontoandroidworkshop_deltahacksinstitute/MainActivity.java
+++ app/src/main/java/ml/deltahacksinstitute/introductiontoandroidworkshop_deltahacksinstitute/MainActivity.java
@@ -1,13 +1,27 @@
 package ml.deltahacksinstitute.introductiontoandroidworkshop_deltahacksinstitute;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonOne = (Button) findViewById(R.id.HelloWorldButton);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (buttonOne.getText().equals("Click Me!")){
+                    buttonOne.setText("Hello World!");
+                } else {
+                    buttonOne.setText("Click Me!");
+                }
+            }
+        });
     }
 }
